c3e862bc19ff188fd96eee311efd9eec641b3008
Shafay <muhammad.shafy99@gmail.com>
2024-07-11 22:07:28 +0500

    utils code

--- src/index.js
+++ src/index.js
@@ -6,42 +6,12 @@ dotenv.config({
     path: "./.env"
 });
 
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const app = express();
-// (async () => {
-//     try {
-//         await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}');    
-//         app.on("error", (error) => {
-//             console.log(error);
-//             throw error
-//         })
-
-//         app.listen(process.env.PORT,() =>{
-//             console.log('App is listening on port ${process.env.PORT}');
-//         })
-
-//     } catch (error) {
-//         console.log(error);
-//         throw error
-//     }
-// })()
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on PORT ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.log(error);
+})
